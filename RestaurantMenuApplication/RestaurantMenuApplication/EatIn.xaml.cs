0827334745dc9661e3441204577aee287e04b0e9
Sagheer <G00357770@gmit.ie>
2018-12-11 15:05:49 +0000

    Init commit

---
+++ RestaurantMenuApplication/RestaurantMenuApplication/EatIn.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RestaurantMenuApplication
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EatIn : ContentPage
+	{
+		public EatIn ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Ordered_Clicked(object sender, EventArgs e)
+        {
+
+            Application.Current.MainPage = new NavigationPage(new Drinks());
+        }
+    }
+}
---
+++ RestaurantMenuApplication/RestaurantMenuApplication/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RestaurantMenuApplication
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        /*
+                async void Eatin_Clicked(object sender, EventArgs e)
+                {
+                     await Navigation.PushAsync(new EatIn());
+                }
+        */
+        private async void Eatin_Clicked(object sender, EventArgs e)
+        {
+
+            Application.Current.MainPage = new NavigationPage(new EatIn());
+        }
+    }
+}
